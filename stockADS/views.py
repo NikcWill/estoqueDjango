150469eda255ef3ee6a57d145e42c4706d0aed36
NikcWill <nwtads03@gmail.com>
2023-10-18 21:56:28 -0300

    elementos for

--- stockADS/views.py
+++ stockADS/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
+from.models import Products
  
 
 def index(request):
 
-  return HttpResponse('Estou no Django')
+  #return HttpResponse('Estou no Django')
+  #from Product select *
+
+  produtos = Products.objects.all()
+  #for produto in produtos:
+    #print(f'{produto.name} + {produto.price}')
+  return render(request, 'pages/index.html', {'produtos':produtos})
